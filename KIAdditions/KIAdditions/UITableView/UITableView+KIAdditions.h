8f620357d65fac0536de79e81a29b862caf9c2cb
smartwalle <smartwalle@gmail.com>
2016-02-24 14:45:50 +0800

    Update UITableView+KIAdditions.h

--- KIAdditions/KIAdditions/UITableView/UITableView+KIAdditions.h
+++ KIAdditions/KIAdditions/UITableView/UITableView+KIAdditions.h
@@ -17,18 +17,25 @@
 - (void)setSeparatorImage:(UIImage *)image;
 
 - (void)addItemToFirst:(id)item withDataSource:(NSMutableArray *)dataSource;
+- (void)addItemToFirst:(id)item withDataSource:(NSMutableArray *)dataSource inSection:(NSInteger)section;
 
 - (void)addItemsToFirst:(NSArray *)items withDataSource:(NSMutableArray *)dataSource;
+- (void)addItemsToFirst:(NSArray *)items withDataSource:(NSMutableArray *)dataSource inSection:(NSInteger)section;
 
 - (void)addItemToLast:(id)item withDataSource:(NSMutableArray *)dataSource;
+- (void)addItemToLast:(id)item withDataSource:(NSMutableArray *)dataSource inSection:(NSInteger)section;
 
 - (void)addItemsToLast:(NSArray *)items withDataSource:(NSMutableArray *)dataSource;
+- (void)addItemsToLast:(NSArray *)items withDataSource:(NSMutableArray *)dataSource inSection:(NSInteger)section;
 
 - (void)addItem:(id)item toIndex:(NSUInteger)index withDataSource:(NSMutableArray *)dataSource;
+- (void)addItem:(id)item toIndex:(NSUInteger)index withDataSource:(NSMutableArray *)dataSource inSection:(NSInteger)section;
 
 - (void)addItems:(NSArray *)items beginWithIndex:(NSUInteger)index withDataSource:(NSMutableArray *)dataSource;
+- (void)addItems:(NSArray *)items beginWithIndex:(NSUInteger)index withDataSource:(NSMutableArray *)dataSource inSection:(NSInteger)section;
 
 - (void)addItems:(NSArray *)items beginWithIndex:(NSUInteger)index itemCountInRow:(NSUInteger)itemCountInRow withDataSource:(NSMutableArray *)dataSource;
+- (void)addItems:(NSArray *)items beginWithIndex:(NSUInteger)index itemCountInRow:(NSUInteger)itemCountInRow withDataSource:(NSMutableArray *)dataSource inSection:(NSInteger)seciton;
 
 - (void)removeItemAtIndexPath:(NSIndexPath *)indexPath withDataSource:(NSMutableArray *)dataSource;
 
